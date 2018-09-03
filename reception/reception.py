8ad3ad5e113325436ee10f8c12852baafac155d5
ekumamait <ericebuluochol@gmail.com>
2018-09-04 00:19:35 +0300

    modify registration to use lists, to append both lists to one and close IO object [(Fixes)#160239804]

--- reception/reception.py
+++ reception/reception.py
@@ -8,32 +8,37 @@ name = input("Enter Name to check ticket status: ")
 """This iterates to check for name given"""
 
 def register_checker(name):
+    vip_list = []
+    ordinary_list = []
+    all_guests = []
+
     f = open('ordinary_list.txt', 'r') 
     ordinary = f.read().split('\n')
-    
+    ordinary_list = ordinary
+
     f = open('vip_list.txt', 'r') 
     vip = f.read().split('\n')
+    vip_list = vip
 
-    for n in ordinary:
+    all_guests.append(ordinary_list)
+    all_guests.append(vip_list)
+
+    f.close()
+
+    for n in all_guests[0]:
         input = n.split()
         if name == n or name == input[0] or name == input[1]:
             return str(n) + ' is ordinary' 
           
-    for n in vip:
+    for n in all_guests[1]:
         input = n.split()
         if name == n or name == input[0] or name == input[1]:
             return str(n) +  ' is vip' 
 
-    return name + ' is not on any list'       
-
+    return name + ' is not on any list' 
+    
 """This returns method"""
-while True: 
-    print(register_checker(name))
-    close = input("Enter Another Name: ")
-    if close == 'No':
-        break
+print(register_checker(name))
 
 
-
-
-             
+        
