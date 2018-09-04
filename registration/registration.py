8dd171256fd0839ff71cfe29485afe4398148c36
ekumamait <ericebuluochol@gmail.com>
2018-09-04 18:01:27 +0300

    fixes

---
+++ registration/registration.py
@@ -0,0 +1,20 @@
+
+class Registration():
+
+    def __init__(self):
+        pass
+
+    def form(self):
+        pass
+
+    def user_name(self):
+        pass
+
+    def user_password(self):
+        pass    
+
+    def submit(self):
+        pass
+
+
+        
---
+++ tests/test_registration.py
@@ -0,0 +1,29 @@
+import unittest
+# from registration import Registration
+
+class RegistrationTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_form(self):
+        pass
+
+    def test_user_name(self):
+        pass
+
+    def test_user_password(self):
+        pass
+
+    def test_user_already_exists(self):
+        pass
+
+    def test_user_doesnt_exist(self):
+        pass    
+
+    def test_if_user_in_list(self):
+        pass              
+
+    def test_submit(self):
+        pass
+              
