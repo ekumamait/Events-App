4c8ef229848f4e9b0e44b84659b676a7ad67eb72
ekumamait <ericebuluochol@gmail.com>
2018-09-05 14:15:37 +0300

    create guest registration class and write failing tests [#160280604]

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,29 +1,46 @@
 import unittest
-# from registration import Registration
+from registration.signup import Registration
 
 class RegistrationTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.register = Registration()
 
     def test_form(self):
-        pass
+        self.assertIsInstance(self.register, Registration)
 
-    def test_user_name(self):
-        pass
+    def test_add_username(self):
+        self.register.username("Jane Doe")
+        self.assertEqual(self.register.username("Jane Doe"), "Jane Doe")    
 
-    def test_user_password(self):
-        pass
+    def test_add_email(self):
+        self.register.email("jane@mail.com")
+        self.assertEqual(self.register.email("Jane"), "jane@mail.com")
 
-    def test_user_already_exists(self):
-        pass
+    def test_add_category(self):
+        self.register.category("vip")    
+        self.assertEqual(self.register.category("John"), "vip")
 
-    def test_user_doesnt_exist(self):
-        pass    
+    def test_missing_key(self):
+        with self.assertRaises(KeyError):
+            self.register.email("")
 
-    def test_if_user_in_list(self):
-        pass              
+    def test_add_ordinary_guest(self):
+        self.register.username("James Safari")
+        self.register.email("james@mail.com")
+        self.register.category("ordinary")
+        self.assertEqual(len(self.register.ordinary_list), 1)        
 
-    def test_submit(self):
-        pass
+    def test_add_vip_guest(self):
+        self.register.username("Tom Jones")
+        self.register.email("tom@mail.com")
+        self.register.category("vip")
+        self.assertEqual(len(self.register.vip_list), 1)
+
+    @unittest.skip("WIP")
+    def test_unknown_methods(self):
+        self.assertEqual(self.register.some_methods(), 1)    
+
+    def tearDown(self):
+        pass      
               
